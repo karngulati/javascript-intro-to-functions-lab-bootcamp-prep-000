811160c5d86eb1851c71a6e1f37b2c27ba669641
Karn Gulati <karngulati@gmail.com>
2018-08-08 22:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,19 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+var love = "I love you, Grandma."
+
 function sayHiToGrandma(shout) {
   return "YES INDEED!"
 }
 
+function sayHiToGrandma(whisper) {
+  return "I can't hear you!"
+}
+
+function sayHiToGrandma(love) {
+  return "I love you, too."
+}
 
 
   
