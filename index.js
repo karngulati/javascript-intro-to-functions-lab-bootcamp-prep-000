51d15ebf6e9cd0f6e3e460398b01e1b94c26ee09
Karn Gulati <karngulati@gmail.com>
2018-08-08 22:44:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function shout(string) {
   return string.toUpperCase()
-};
+}
 
 function whisper(string) {
   return string.toLowerCase()
-};
+}
 
 function logShout(string) {
   console.log(string.toUpperCase())
-};
+}
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
-};
+}
 
 
 function sayHiToGrandma(string) {
@@ -20,8 +20,10 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase === string) {
     return "YES INDEED!"
-  } else if (string.)
-
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
 
 
 
