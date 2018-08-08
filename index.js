b45d63480d6bdf734259c6db7a67d2a3e89e3eb8
Karn Gulati <karngulati@gmail.com>
2018-08-08 22:30:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string === toUpperCase) {
+  if (string === ) {
     return "YES INDEED!"
-  } else if (string === toLowerCase) {
+  } else if (string === ) {
     return "I can't hear you!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
